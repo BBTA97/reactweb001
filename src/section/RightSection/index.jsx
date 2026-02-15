b1114796fee3d60a39542d1e7b5792cc438083cd
BBTA97 <patcharada.kh@ku.th>
2026-02-15 12:08:56 +0700

    update

---
+++ src/section/RightSection/index.jsx
@@ -0,0 +1,17 @@
+import About from "../About"
+
+const RightSection = () => {
+  return (
+    <div className="grid gap-4 px-5">
+      <About />
+      <div>Experience</div>
+      <div>Experience</div>
+      <div>Experience</div>
+      <div>Experience</div>
+      <div>Experience</div>
+      <div>Experience</div>
+    </div>
+  )
+}
+
+export default RightSection
